6e932e84ac302367c9b56e983b9996f456aac4f2
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-09-29 20:08:13 +0200

    Add assert macros

---
+++ assert.hpp
@@ -0,0 +1,89 @@
+//=======================================================================
+// Copyright (c) 2013-2014 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#ifndef CPP_UTILS_ASSERT_HPP
+#define CPP_UTILS_ASSERT_HPP
+
+#include <iostream>
+
+#ifdef CPP_UTILS_ASSERT_EXCEPTION
+#include <stdexcept>
+#endif
+
+#include "likely.hpp"
+
+#define cpp_unused(x) ((void)x)
+
+#ifdef NDEBUG
+
+#define cpp_assert(condition, message) ((void)0)
+
+#if defined __clang__
+
+#if __has_builtin(__builtin_unreachable)
+#define cpp_unreachable(message) __builtin_unreachable();
+#else
+#define cpp_unreachable(message) ((void)0)
+#endif //__has_builtin(__builtin_unreachable)
+
+#elif defined __GNUC__
+
+#define cpp_unreachable(message) __builtin_unreachable();
+
+#endif //__clang__
+
+#else
+
+#ifdef CPP_UTILS_ASSERT_EXCEPTION
+
+#define cpp_assert(condition, message) if(likely(condition)) \
+    ((void)0); \
+    else throw std::runtime_error("Assertion failed");
+
+#else
+
+#define cpp_assert(condition, message) (likely(condition) \
+    ? ((void)0) \
+    : ::cpp::assertion::detail::assertion_failed_msg(#condition, message, \
+    __PRETTY_FUNCTION__, __FILE__, __LINE__))
+
+#endif
+
+#if defined __clang__
+
+#if __has_builtin(__builtin_unreachable)
+#define cpp_unreachable(message) cpp_assert(false, message); __builtin_unreachable();
+#else
+#define cpp_unreachable(message) cpp_assert(false, message);
+#endif //__has_builtin(__builtin_unreachable)
+
+#elif defined __GNUC__
+
+#define cpp_unreachable(message) cpp_assert(false, message); __builtin_unreachable();
+
+#endif //__clang__
+
+#endif //NDEBUG
+
+namespace cpp {
+namespace assertion {
+namespace detail {
+
+template< typename CharT >
+void assertion_failed_msg(const CharT* expr, const char* msg, const char* function, const char* file, long line){
+    std::cerr
+        << "***** Internal Program Error - assertion (" << expr << ") failed in "
+        << function << ":\n"
+        << file << '(' << line << "): " << msg << std::endl;
+    std::abort();
+}
+
+} // end of detail namespace
+} // end of assertion namespace
+} // end of detail namespace
+
+#endif //CPP_UTILS_ASSERT_HPP
