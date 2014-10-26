db62260a4369f0340c53c87ee13907949b581786
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-10-26 16:49:39 +0100

    Finish wait()

--- parallel.hpp
+++ parallel.hpp
@@ -40,25 +40,34 @@ void parallel_foreach_i(const Container& container, Functor&& fun){
     //No need to wait for the futures, the destructor will do it for us
 }
 
+enum class thread_status {
+    WAITING,
+    WORKING
+};
+
 struct default_thread_pool {
 private:
     std::vector<std::thread> threads;
+    std::vector<thread_status> status;
     std::deque<std::function<void()>> tasks;
-    std::mutex lock;
+    std::mutex main_lock;
     std::condition_variable condition;
     volatile bool stop_flag = false;
 
 public:
-    default_thread_pool(std::size_t n){
+    default_thread_pool(std::size_t n) : status(n, thread_status::WAITING) {
         for(std::size_t t = 0; t < n; ++t){
             threads.emplace_back(
-                [&tasks = this->tasks, &lock = this->lock, &condition = this->condition, &stop_flag = this->stop_flag]
+                [t, &status = this->status, &tasks = this->tasks, &main_lock = this->main_lock, &condition = this->condition, &stop_flag = this->stop_flag]
                 {
                     while(true){
                         std::function<void()> task;
 
                         {
-                            std::unique_lock<std::mutex> ulock(lock);
+                            std::unique_lock<std::mutex> ulock(main_lock);
+
+                            status[t] = thread_status::WAITING;
+
                             condition.wait(ulock, [&stop_flag, &tasks]{return stop_flag || !tasks.empty(); });
 
                             if(stop_flag && tasks.empty()){
@@ -67,6 +76,8 @@ public:
 
                             task = std::move(tasks.front());
                             tasks.pop_front();
+
+                            status[t] = thread_status::WORKING;
                         }
 
                         task();
@@ -79,7 +90,7 @@ public:
 
     ~default_thread_pool(){
         {
-            std::unique_lock<std::mutex> ulock(lock);
+            std::unique_lock<std::mutex> ulock(main_lock);
             stop_flag = true;
         }
 
@@ -93,10 +104,20 @@ public:
     //TODO DO better than busy waiting
     void wait(){
         while(true){
-            std::unique_lock<std::mutex> ulock(lock);
+            std::unique_lock<std::mutex> ulock(main_lock);
 
             if(tasks.empty()){
-                return;
+                bool still_working = false;
+                for(auto s : status){
+                    if(s == thread_status::WORKING){
+                        still_working = true;
+                        break;
+                    }
+                }
+
+                if(!still_working){
+                    return;
+                }
             }
         }
     }
@@ -108,7 +129,7 @@ public:
             );
 
         {
-            std::unique_lock<std::mutex> ulock(lock);
+            std::unique_lock<std::mutex> ulock(main_lock);
 
             if(stop_flag){
                 throw std::runtime_error("enqueue on stopped ThreadPool");
