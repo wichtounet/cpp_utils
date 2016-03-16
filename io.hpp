c8ba6def01d134038df509c136a771838f4e3f93
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-03-16 14:49:21 +0100

    Binary I/O utilities

---
+++ io.hpp
@@ -0,0 +1,48 @@
+//=======================================================================
+// Copyright (c) 2013-2016 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+/*!
+ * \file
+ * \brief Contains functions to write to binary files
+ */
+
+#ifndef CPP_UTILS_IO_HPP
+#define CPP_UTILS_IO_HPP
+
+#include <iosfwd>
+
+namespace cpp {
+
+//Binary I/O utility functions
+
+template <typename T>
+void binary_write(std::ostream& os, const T& v) {
+    os.write(reinterpret_cast<const char*>(&v), sizeof(v));
+}
+
+template <typename Container>
+void binary_write_all(std::ostream& os, const Container& c) {
+    for (auto& v : c) {
+        binary_write(os, v);
+    }
+}
+
+template <typename T>
+void binary_load(std::istream& is, T& v) {
+    is.read(reinterpret_cast<char*>(&v), sizeof(v));
+}
+
+template <typename Container>
+void binary_load_all(std::istream& is, Container& c) {
+    for (auto& v : c) {
+        binary_load(is, v);
+    }
+}
+
+} //end of the cpp namespace
+
+#endif //CPP_UTILS_IO_HPP
