f94efe66bdc4944b11bc42300d4bf9f6b97c9fff
Baptiste Wicht <baptiste.wicht@gmail.com>
2018-01-31 08:18:14 +0100

    Fix missing include

--- algorithm.hpp
+++ algorithm.hpp
@@ -13,8 +13,9 @@
 #ifndef CPP_UTILS_ALGORITHM_HPP
 #define CPP_UTILS_ALGORITHM_HPP
 
-#include <vector>    //for vector_transform (std::vector)
-#include <algorithm> //for vector_transform (std::transform)
+#include <algorithm> // for vector_transform (std::transform)
+#include <random>    // for shuffle algorithms
+#include <vector>    // for vector_transform (std::vector)
 
 #include "assert.hpp"
 
