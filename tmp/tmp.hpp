80af903337e53ba5facf554e6dd602683894f02d
Baptiste Wicht <baptiste.wicht@gmail.com>
2022-08-04 06:27:18 +0200

    Restore macros

--- tmp/tmp.hpp
+++ tmp/tmp.hpp
@@ -321,4 +321,27 @@ struct conditional_template_type_constant_c<false, V1, V2> {
 
 } //end of namespace cpp
 
+#define HAS_MEM_FUNC(func, name)                                              \
+template<typename T, typename Sign>                                           \
+struct name {                                                                 \
+    typedef char yes[1];                                                      \
+    typedef char no [2];                                                      \
+    template <typename U, U> struct type_check;                               \
+    template <typename _1> static yes &chk(type_check<Sign, &_1::func> *);    \
+    template <typename   > static no  &chk(...);                              \
+    static constexpr const bool value = sizeof(chk<T>(0)) == sizeof(yes);     \
+}
+
+#define HAS_STATIC_FIELD(field, name)                                                       \
+template <class T>                                                                          \
+class name {                                                                                \
+    template<typename U, typename =                                                         \
+        typename std::enable_if<!std::is_member_pointer<decltype(&U::field)>::value>::type> \
+    static std::true_type check(int);                                                       \
+    template <typename>                                                                     \
+    static std::false_type check(...);                                                      \
+public:                                                                                     \
+    static constexpr const bool value = decltype(check<T>(0))::value;                       \
+}
+
 #endif //CPP_UTILS_TMP_HPP
